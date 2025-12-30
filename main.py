9015e543822ca29899aa7d51a60a42d547160f3f
kitta65 <26474260+kitta65@users.noreply.github.com>
2025-12-30 18:24:02 +0900

    use sync version

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import asyncio
 import streamlit as st
 from constants.role import Role
 from agents.root import get_root_agent
@@ -12,34 +11,30 @@ def init():
 (agent,) = init()
 
 
-async def main():
-    # ----- config -----
-    st.set_page_config(
-        page_title="PoC of AI Analyzer",
+# ----- config -----
+st.set_page_config(
+    page_title="PoC of AI Analyzer",
+)
+st.title("PoC of AI Analyzer")
+
+# ----- initialize -----
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# ----- chat -----
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("Ask anything about data analysis"):
+    with st.chat_message(Role.USER.value):
+        st.markdown(prompt)
+    st.session_state.messages.append({"role": Role.USER.value, "content": prompt})
+
+    with st.chat_message(Role.ASSISTANT.value):
+        stream = agent.run_stream_sync(prompt)
+        response = st.write_stream(stream.stream_text(delta=True))
+
+    st.session_state.messages.append(
+        {"role": Role.ASSISTANT.value, "content": response}
     )
-    st.title("PoC of AI Analyzer")
-
-    # ----- initialize -----
-    if "messages" not in st.session_state:
-        st.session_state.messages = []
-
-    # ----- chat -----
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
-
-    if prompt := st.chat_input("Ask anything about data analysis"):
-        with st.chat_message(Role.USER.value):
-            st.markdown(prompt)
-        st.session_state.messages.append({"role": Role.USER.value, "content": prompt})
-
-        with st.chat_message(Role.ASSISTANT.value):
-            response = agent.run_sync(prompt)
-            st.write(response.output)
-        st.session_state.messages.append(
-            {"role": Role.ASSISTANT.value, "content": response.output}
-        )
-
-
-if __name__ == "__main__":
-    asyncio.run(main())
