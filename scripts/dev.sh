4606d585786c3df76850f88b81efb536db818f9c
kitta65 <26474260+kitta65@users.noreply.github.com>
2025-12-30 11:31:18 +0900

    start cube

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -2,4 +2,17 @@
 set -euo pipefail
 cd $(git rev-parse --show-toplevel)
 
+cube_container_name="cube-dev-container"
+docker container run \
+  -d \
+  --rm \
+  --name $cube_container_name \
+  -e CUBEJS_DB_TYPE=bigquery \
+  -e CUBEJS_DEV_MODE=true \
+  -e CUBEJS_DB_BQ_PROJECT_ID=$(gcloud config get-value project) \
+  -e CUBEJS_DB_BQ_CREDENTIALS=$(cat "${HOME}/.config/gcloud/application_default_credentials.json" | base64 | tr -d '\n') \
+  -p 4000:4000 \
+  -p 15432:15432 \
+  -v ./cube:/cube/conf \
+  cubejs/cube:v1.3.54
 uv run streamlit run --server.headless true main.py
