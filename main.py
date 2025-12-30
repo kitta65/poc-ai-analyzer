801b8dd4d5167cb44c7c78bfffc6897930466f9a
kitta65 <26474260+kitta65@users.noreply.github.com>
2025-12-30 10:25:11 +0900

    add chat ui

---
+++ main.py
@@ -0,0 +1,29 @@
+import streamlit as st
+from constants.role import Role
+
+
+# ----- config -----
+st.set_page_config(
+    page_title="PoC of AI Analyzer",
+)
+st.title("PoC of AI Analyzer")
+
+# ----- initialize -----
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# ----- chat -----
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("Ask anything about data analysis"):
+    with st.chat_message(Role.USER.value):
+        st.markdown(prompt)
+    st.session_state.messages.append({"role": Role.USER.value, "content": prompt})
+
+    with st.chat_message(Role.ASSISTANT.value):
+        response = st.write_stream("Hi I'm your AI assistant!")
+    st.session_state.messages.append(
+        {"role": Role.ASSISTANT.value, "content": response}
+    )
