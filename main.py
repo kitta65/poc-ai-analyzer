6cdc9f10219bc05c8da8af71cd76ff3a5e0275b5
kitta65 <26474260+kitta65@users.noreply.github.com>
2025-12-30 16:03:45 +0900

    enable async functions

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import asyncio
 import streamlit as st
 from constants.role import Role
 from agents.root import get_root_agent
@@ -11,29 +12,34 @@ def init():
 (agent,) = init()
 
 
-# ----- config -----
-st.set_page_config(
-    page_title="PoC of AI Analyzer",
-)
-st.title("PoC of AI Analyzer")
-
-# ----- initialize -----
-if "messages" not in st.session_state:
-    st.session_state.messages = []
-
-# ----- chat -----
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
-
-if prompt := st.chat_input("Ask anything about data analysis"):
-    with st.chat_message(Role.USER.value):
-        st.markdown(prompt)
-    st.session_state.messages.append({"role": Role.USER.value, "content": prompt})
-
-    with st.chat_message(Role.ASSISTANT.value):
-        response = agent.run_sync(prompt)
-        st.write(response.output)
-    st.session_state.messages.append(
-        {"role": Role.ASSISTANT.value, "content": response.output}
+async def main():
+    # ----- config -----
+    st.set_page_config(
+        page_title="PoC of AI Analyzer",
     )
+    st.title("PoC of AI Analyzer")
+
+    # ----- initialize -----
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+
+    # ----- chat -----
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+    if prompt := st.chat_input("Ask anything about data analysis"):
+        with st.chat_message(Role.USER.value):
+            st.markdown(prompt)
+        st.session_state.messages.append({"role": Role.USER.value, "content": prompt})
+
+        with st.chat_message(Role.ASSISTANT.value):
+            response = agent.run_sync(prompt)
+            st.write(response.output)
+        st.session_state.messages.append(
+            {"role": Role.ASSISTANT.value, "content": response.output}
+        )
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
